7b640e13f3b6ec707d3f689c2335e5710b719b1b
NilKad <kadulin@gmail.com>
2022-10-27 00:20:40 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,8 @@ import user from './Profile/user.json';
 import friends from './FriendList/friends.json';
 import statisticsData from './Statistics/data.json';
 import transactions from './TransactionHistory/transactions.json';
-// import statsData from './Statistics/';
 import { Statistics } from './Statistics/Statistics';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-// console.log('user: ', user);
-// console.log('statisticsData: ', statisticsData);
 
 export const App = () => {
   return (
